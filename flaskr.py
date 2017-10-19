a170c9a142524e2b3df35efd5fcecc44ea343055
AadityaOfficial <aadityasuri33@gmail.com>
2017-10-19 09:25:02 +0530

    server completed with required endpoints

--- flaskr.py
+++ flaskr.py
@@ -1,102 +1,187 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file, g
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, load_only
 from models import Base,User,Election
+from werkzeug.exceptions import BadRequest
+from urllib import urlretrieve
+from passlib.apps import custom_app_context as pwd_context
+from flask_httpauth import HTTPBasicAuth
+import json
 
+#new imports
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+from flask import make_response
+import requests
 
+auth=HTTPBasicAuth()
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
 engine = create_engine('sqlite:///app.db')
 Base.metadata.bind = engine
 
+
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
-
 app = Flask(__name__) 
-
 # Create the appropriate app.route functions, 
 #test and see if they work
 
-
 #Make an app.route() decorator here
 @app.route("/")
+@app.route('/token')
+@auth.login_required
+def get_auth_token():
+    token = g.user.generate_auth_token()
+    return jsonify({'token': token.decode('ascii')})
+
+@app.route("/register", methods = ['POST'])
+def newUser():
+  data=request.get_json(force=True)
+  username=data['username']
+  password=data['password']
+  if username is None or password is None:
+    abort(400) #missing arguments
+  if session.query(User).filter_by(username=username).first() is not None:
+    abort(400)
+  user=User(username=username)
+  user.hash_password(password)
+  session.add(user)
+  session.commit()
+  return jsonify({'username':user.username}),201
+
+@app.route("/login", methods = ['POST'])
+@auth.login_required
+def oldUser():
+  result=False
+  data=request.get_json(force=True)
+  username=data['username']
+  password=data['password']
+  if username is None or password is None:
+    abort(400) #missing arguments
+  if session.query(User).filter_by(username=username).first() is not None:
+    result=verify_password(username, password)
+  
+  return jsonify({'username':username,'valid':result}),201
+
+@app.route("/userdata/<string:username>", methods = ['GET','PUT'])
+@auth.login_required
+def userData(username):
+    data=request.get_json(force=True)
+    if request.method == 'GET':
+      return getUserData()
+    elif request.method == 'PUT':
+      positive_vote=data['positive_vote']
+      print positive_vote
+      negative_vote=data['negative_vote']
+      return updateVotes(username,positive_vote,negative_vote)
+
+@app.route("/alluserdata/<string:username>", methods = ['GET','PUT'])
+@auth.login_required
+def changeUserData(username):
+  if request.method=='GET':
+    return getUserProfile(username)
+  elif request.method=='PUT':
+    data=request.get_json(force=True)
+    newUsername=data['newUsername']
+    gender=data['gender']
+    mobile_number=data['mobile_number']
+    return changeData(username,gender,mobile_number,newUsername)
+
+
+@auth.verify_password
+def verify_password(username_or_token, password):
+    #Try to see if it's a token first
+    user_id = User.verify_auth_token(username_or_token)
+    if user_id:
+        user = session.query(User).filter_by(id = user_id).one()
+    else:
+        user = session.query(User).filter_by(username = username_or_token).first()
+        if not user or not user.verify_password(password):
+            return False
+    g.user = user
+    return True
+
+
+@app.route("/election/<string:category>",methods=['GET'])
+@auth.login_required
+def categoryFunction(category):
+  return getElectionByCategory(category)
+
 @app.route("/elections", methods = ['GET', 'POST'])
+@auth.login_required
 def electionFunction():
   if request.method == 'GET':
     return getAllElections()
   elif request.method == 'POST':
-    #Call the method to make a new puppy
+    #Call the method to make a new election
     print "Making a New election"
-    name = request.args.get('name', '')
-    desc_h1 = request.args.get('desc_h1', '')
-    desc_h2=request.args.get('desc_h2', '')
-    desc_h3=request.args.get('desc_h3', '')
-    desc_p1=request.args.get('desc_p1', '')
-    desc_p2=request.args.get('desc_p2', '')
-    desc_p3=request.args.get('desc_p3', '')
-    category = request.args.get('category', '')
-    start_time=request.args.get('start_time', '')
-    end_time=request.args.get('end_time', '')
-    in_favour=request.args.get('in_favour', '')
-    not_in_favour=request.args.get('not_in_favour', '')
-    status=request.args.get('status', '')
-    total_votes=request.args.get('total_votes', '')
-    return makeANewElection(name, desc_h1,desc_h2,desc_h3,desc_p1,desc_p2,desc_p3,category,start_time,end_time,in_favour,not_in_favour,status,total_votes)
+    data=request.get_json(force=True)
+    name=data['name']
+    heading=data['heading']
+    para=data['para']
+    category=data['category']
+    start_time=data['start_time']
+    end_time=data['end_time']
+    in_favour=data['in_favour']
+    not_in_favour=data['not_in_favour']
+    status=data['status']
+    total_votes=data['total_votes']
+    return makeANewElection(name,heading,para,category,start_time,end_time,in_favour,not_in_favour,status,total_votes)
  
 #Make another app.route() decorator here that takes in an integer id in the URI
 @app.route("/elections/<int:id>", methods = ['GET', 'PUT', 'DELETE'])
-#Call the method to view a specific puppy
+#Call the method to view a specific election
 def electionFunctionId(id):
   if request.method == 'GET':
     return getElection(id)
     
-#Call the method to edit a specific puppy  
+#Call the method to edit a specific election  
   elif request.method == 'PUT':
-    name = request.args.get('name', '')
-    desc_h1 = request.args.get('desc_h1', '')
-    desc_h2=request.args.get('desc_h2', '')
-    desc_h3=request.args.get('desc_h3', '')
-    desc_p1=request.args.get('desc_p1', '')
-    desc_p2=request.args.get('desc_p2', '')
-    desc_p3=request.args.get('desc_p3', '')
-    category = request.args.get('category', '')
-    start_time=request.args.get('start_time', '')
-    end_time=request.args.get('end_time', '')
-    status=request.args.get('status', '')
-    return updateElection(id,name, desc_h1,desc_h2,desc_h3,desc_p1,desc_p2,desc_p3,category,start_time,end_time,status)
+    data=request.get_json(force=True)
+    name=data['name']
+    heading=data['heading']
+    para=data['para']
+    category=data['category']
+    start_time=data['start_time']
+    end_time=data['end_time']
+    status=data['status']
+    return updateElection(id,name, heading,para,category,start_time,end_time,status)
     
- #Call the method to remove a puppy 
+ #Call the method to remove a election 
   elif request.method == 'DELETE':
     return deleteElection(id)
 
+#functions starting******************************************************************
+#functions starting *****************************************************************
 def getAllElections():
   elections = session.query(Election).all()
   return jsonify(Elections=[i.serialize for i in elections])
+
+def getUserData(username):
+  user=session.query(User).filter_by(username=username).one()
+  return jsonify({'total_votes':user.total_votes,'positive_votes':user.votes_in_favour,'negative_vote':user.votes_against})
 
 def getElection(id):
   election = session.query(Election).filter_by(election_id = id).one()
   return jsonify(Election=election.serialize) 
   
-def makeANewElection(name, desc_h1,desc_h2,desc_h3,desc_p1,desc_p2,desc_p3,category,start_time,end_time,in_favour,not_in_favour,status,total_votes):
-  election = Election(name = name, desc_h1 = desc_h1, desc_h2=desc_h2, desc_h3=desc_h3, desc_p1=desc_p1, desc_p2=desc_p2, desc_p3= desc_p3, category=category, start_time=start_time, end_time=end_time, in_favour=in_favour, not_in_favour=not_in_favour, status=status, total_votes=total_votes)
+def makeANewElection(name, heading,para,category,start_time,end_time,in_favour,not_in_favour,status,total_votes):
+  election = Election(name = name, heading = heading, para=para, category=category, start_time=start_time, end_time=end_time, in_favour=in_favour, not_in_favour=not_in_favour, status=status, total_votes=total_votes)
   session.add(election)
   session.commit()
   return jsonify(Election=election.serialize)
 
-def updateElection(id,name, desc_h1,desc_h2,desc_h3,desc_p1,desc_p2,desc_p3,category,start_time,end_time,status):
+def updateElection(id,name, heading,para,category,start_time,end_time,status):
   election = session.query(Election).filter_by(election_id = id).one()
   if not name:
     election.name = name
-  if not desc_h1:
-    election.desc_h1 = desc_h1
-  if not desc_h2:
-    election.desc_h2 = desc_h2
-  if not desc_h3:
-    election.desc_h3 = desc_h3
-  if not desc_p1:
-    election.desc_p1 = desc_p1
-  if not desc_p2:
-    election.desc_p2 = desc_p2
-  if not desc_p3:
-    election.desc_p3 = desc_p3
+  if not heading:
+    election.heading = heading
+  if not para:
+    election.para = para
   if not category:
     election.category=category
   if not start_time:
@@ -109,11 +194,56 @@ def updateElection(id,name, desc_h1,desc_h2,desc_h3,desc_p1,desc_p2,desc_p3,cate
   session.commit()
   return "Updated an election with id %s" % id
 
+def updateVotes(username,positive_vote,negative_vote):
+  user=session.query(User).filter_by(username=username).one()
+  if positive_vote =="True":
+    if user.votes_in_favour is None:
+      user.votes_in_favour=0
+    user.votes_in_favour=user.votes_in_favour+1
+  if not negative_vote and negative_vote == "True":
+    if user.votes_against is None:
+      user.votes_against=0
+    user.votes_against=user.votes_against+1
+  if user.total_votes is None:
+    user.total_votes=0
+  else:
+    user.total_votes=user.total_votes+1
+  session.add(user)
+  session.commit()
+  return jsonify({'total_votes':user.total_votes,'positive_votes':user.votes_in_favour,'negative_vote':user.votes_against})
+
 def deleteElection(id):
   election = session.query(Election).filter_by(election_id = id).one()
   session.delete(election)
   session.commit()
   return "Removed election with id %s" % id
+
+def getElectionByCategory(category):
+  elections=session.query(Election).filter_by(category=category).all()
+  return jsonify(Elections=[k.serialize for k in elections])
+
+def getUserProfile(username):
+    user=session.query(User).filter_by(username=username).one()
+    return jsonify({'username':user.username,'gender':user.gender,'mobile_number':user.mobile_number,'total_votes':user.total_votes,'positive_votes':user.votes_in_favour,'negative_votes':user.votes_against,'role':user.role})
+
+
+def changeData(username,gender,mobile_number,newUsername):
+  user=session.query(User).filter_by(username=username).one()
+  if newUsername is not None:
+    if session.query(User).filter_by(username=newUsername).first() is not None:
+      raise BadRequest()
+    else:
+      user.username=newUsername
+  if gender is not None:
+    user.gender=gender
+  if mobile_number is not None:
+    user.mobile_number=mobile_number
+  session.add(user)
+  session.commit()
+  return jsonify({'username':user.username,'gender':user.gender,'mobile_number':user.mobile_number,'total_votes':user.total_votes,'positive_votes':user.votes_in_favour,'negative_votes':user.votes_against,'role':user.role})
+
+
+
 
 
 if __name__ == '__main__':
--- models.py
+++ models.py
@@ -1,30 +1,64 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-
 Base = declarative_base()
+from passlib.apps import custom_app_context as pwd_context
+import random,string 
+from itsdangerous import (TimedJSONWebSignatureSerializer as Serializer, BadSignature, SignatureExpired)
+secret_key=''.join(random.choice(string.ascii_uppercase+string.digits) for x in xrange(32))
 
+#username is email id
 class User(Base):
     __tablename__ = 'user'
 
 
-    user_name =Column(String(80), nullable = False)
-    user_id = Column(Integer, primary_key = True)
-    role = Column(Boolean, nullable=False)
-    email=Column(String(200), nullable=False)
-    votes=Column(String(500), nullable=False)
-    image=Column(String(200), nullable=False)
+    username =Column(String(80),index=True)
+    id = Column(Integer, primary_key = True)
+    password_hash=Column(String(100))
+    gender=Column(String(32))
+    mobile_number=Column(String(15))
+    total_votes=Column(Integer)
+    votes_in_favour=Column(Integer)
+    votes_against=Column(Integer)
+    role = Column(Integer)
+    userpin=Column(String(200))
+   
+    
+    def hash_password(self,password):
+      self.password_hash=pwd_context.encrypt(password)
+    def verify_password(self,password):
+      return pwd_context.verify(password,self.password_hash)
+    def generate_auth_token(self, expiration=600):
+      s = Serializer(secret_key, expires_in = expiration)
+      return s.dumps({'id': self.id })
+    @staticmethod
+    def verify_auth_token(token):
+      s = Serializer(secret_key)
+      try:
+        data = s.loads(token)
+      except SignatureExpired:
+        #Valid Token, but expired
+        return None
+      except BadSignature:
+        #Invalid Token
+        return None
+      user_id = data['id']
+      return user_id
 
     @property
     def serialize(self):
        """Return object data in easily serializeable format"""
        return {
-       		'id': self.user_id,
-           'name': self.user_name,
-           'role' : self.description,
-           'email':self.email,
+       		'id': self.userid,
+           'username': self.username,
+           'gender':self.gender,
+           'total_votes':self.total_votes,
+           'votes_in_favour':self.votes_in_favour,
+           'votes_against':self.votes_against,
+           'mobile_number':slef.mobile_number,
+           'role' : self.role,
            'votes':self.votes,
-           'image':self.image
+           'userpin':self.userpin,
        }
  
 class Election(Base):
@@ -32,12 +66,8 @@ class Election(Base):
 
 
     name =Column(String(80), nullable = False)
-    desc_h1=Column(String(200), nullable=False)
-    desc_h2=Column(String(200), nullable=False)
-    desc_h3=Column(String(200), nullable=False)
-    desc_p1=Column(String(500), nullable=False)
-    desc_p2=Column(String(500), nullable=False)
-    desc_p3=Column(String(500), nullable=False)
+    heading=Column(String(200), nullable=False)
+    para=Column(String(500), nullable=False)
     election_id = Column(Integer, primary_key = True, )
     category = Column(String(100), nullable=False)
     start_time=Column(String(100),nullable=False)
@@ -53,12 +83,8 @@ class Election(Base):
        return {
           'id': self.election_id,
            'name': self.name,
-           'description_header1' : self.desc_h1,
-            'description_header2' : self.desc_h2,
-            'description_header3' : self.desc_h3,
-            'description_para1' : self.desc_p1,
-            'description_para2' : self.desc_p2,
-            'description_para3' : self.desc_p3,
+           'heading' : self.heading,
+            'paragraph' : self.para,
             'category' :self.category,
             'start_time' :self.start_time,
             'end_time' :self.end_time,
